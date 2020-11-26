75444ba23251c04d9b872c63aef5e6dda871cae9
thals7 <thals_7@naver.com>
2020-11-26 21:20:12 +0900

    201126 data_structure polynomial ing

--- data_structure/practice/polynomial.c
+++ data_structure/practice/polynomial.c
@@ -256,3 +256,95 @@ void erase_blanks(char expression[])
     strcpy(expression, compressed);
     return expression;
 }
+
+Polynomial *create_by_parse_polynomial(char name, char *body) // 다항식의 우변을 해석해서 하나의 다항식 객체를 만들어 그 주소를 return해주는 함수
+{
+    Polynomial *ptr_poly = create_polynomial_instance(name);
+    int i = 0, begin_term = 0;
+    while (i<strlen(body))
+    {
+        if (body[i] == '+' || body[i] == '-') i++;
+        while ( i<strlen(body) && body[i] != '+' && body[i] != '-' ) i++;
+        int result = parse_and_add_term(body, begin_term, i , ptr_poly); // body[begin_term, i) (begin_term부터 i 직전) 까지가 하나의 항. 이 항을 인식해서 다항식에 추가
+        if (result == 0)
+        {
+            destroy_polynomial(ptr_poly);
+            return NULL; // 잘못된 항일 경우 0을 반환하고, 그런 경우 만들고 있던 다항식 자체를 destroy함
+        }
+        begin_term = i; // 다음 항의 시작 위치는 i
+    }
+    return ptr_poly;
+}
+
+Term *parse_and_add_term(char *expr, int begin, int end, Polynomial *p_poly)
+{
+    int i = begin;
+    int sign_coef = 1, coef = 0, expo = 1; // sign_coef는 계수의 부호
+    if (expr[i] == '+') i++;
+    else if (expr[i] == '-')
+    {
+        sign_coef = -1; // 기호로부터 계수의 부호를 결정
+        i++;
+    }
+    while ( i < end && expr[i] >= '0' && expr[i] <= '9' )
+    {
+        coef = coef*10 + (int)(expr[i]-'0');
+        i++;
+    } // 계수의 절댓값 계산
+    if ( coef == 0 ) coef = sign_coef; // coef가 0인 경우 계수는 0이 아니라 1 혹은 -1임
+    else coef *=sign_coef;
+    if ( i >= end )
+    {
+        add_term(coef, 0, p_poly); // 더이상 항을 구성하는 문자가 없다면 상수항이라는 의미
+        return 1;
+    }
+    if (expr[i] != 'x') return 0; // 계수 다음에 x가 아닌 다른 문자가 등장해서는 안됨
+    i++;
+    if ( i >= end )
+    {
+        add_term(coef, 1, p_poly);
+        return 1; // 계수 다음에 x가 나오고 문자열이 끝난다면 1차항이라는 의미
+    }
+    if ( expr[i] != '^' ) return 0; // x 다음에 ^가 아닌 다른 문자가 등장해서는 안됨
+    i++;
+    expo = 0;
+    while ( i < end && expr[i] >= '0' && expr[i] <= '9' )
+    {
+        expo = expo*10 + (int)(expr[i]-'0');
+        i++;
+    } // 지수 부분 계산
+    add_term(coef, expo, p_poly);
+    return 1;
+}
+
+void insert_polynomial(Polynomial *ptr_poly)
+{
+    for ( int i = 0; i < n; i++ )
+    {
+        if (polys[i]->name == ptr_poly->name)
+        {
+            destroy_polynomial(polys[i]); // 다항식을 덮어쓸 경우에는 기존의 다항식 객체를 free 시켜주어야 함
+            polys[i] = ptr_poly;
+            return;
+        }
+    }
+    polys[n++] = ptr_poly;
+}
+
+void destroy_polynomial(Polynomial *ptr_poly)
+{
+    if ( ptr_poly == NULL ) return;
+    Term *t = ptr_poly->first, *tmp;
+    while ( t != NULL )
+    {
+        tmp = t;
+        t = t->next;
+        free(tmp); // 다항식에 속한 모든 항들을 free시킴
+    } 
+    free(ptr_poly); // ptr_poly가 가리키는 다항식 객체를 free시킴
+}
+
+Polynomial *create_by_parse_polynomial(char name, char f, char g)
+{
+
+} // f(name) = g + h 새로운 empty 다항식을 만든 후 두 다항식의 모든 항들을 add(add_term) 해주면 됨
